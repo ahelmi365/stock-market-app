3ab1034df7271aaa987455fc742dcfd8a1cc593a
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 20:54:40 +0200

    fix: update Tickers component to handle scrolling

--- src/components/Tickers/Tickers.tsx
+++ src/components/Tickers/Tickers.tsx
@@ -3,22 +3,25 @@ import useTickers from "./useTickers";
 import Card from "@components/common/Card/Card";
 
 const Tickers = () => {
-  const { isPending, error, data } = useTickers();
-  console.log(data);
-  if (error) throw error;
-  if (isPending) return <div>Loading...</div>;
-  const renderedTickers = data?.results.map((ticker) => (
-    <div className="col-sm-12 col-md-6 col-lg-4" key={ticker.ticker}>
-      <Card title={ticker.ticker} text={ticker.name}>
-        <p>
-          {ticker.currency_symbol} {ticker.base_currency_symbol}
-        </p>
-      </Card>
-    </div>
-  ));
+  const { data, error, isFetchingNextPage, status } = useTickers();
+  if (status === "pending") return <div>Loading...</div>;
+  if (status === "error") throw error;
+
+  const renderedTickers = data?.pages.flatMap((page) =>
+    page.results.map((ticker) => (
+      <div className="col-sm-12 col-md-6 col-lg-4" key={ticker.ticker}>
+        <Card title={ticker.ticker} text={ticker.name}>
+          <p>
+            {ticker.currency_symbol} {ticker.base_currency_symbol}
+          </p>
+        </Card>
+      </div>
+    ))
+  );
   return (
     <div className="row g-3">
       {renderedTickers}
+      {isFetchingNextPage && <div>Loading more...</div>}
     </div>
   );
 };
