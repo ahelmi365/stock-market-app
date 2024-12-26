4a4c56e16e0c9a586643fbdcb7c77de72bb994f4
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 15:41:57 +0200

    refactor MainLayout component for improved readability and structure

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,16 @@
+import { Footer, Header } from "@components/common";
 import { Container } from "react-bootstrap";
-import styles from "./styles.module.css";
-import {Header, Footer} from "@components/common"
 import { Outlet } from "react-router-dom";
+import styles from "./styles.module.css";
 const { container, wrapper } = styles;
 const MainLayout = () => {
+
+ 
   return (
     <Container className={container}>
       <Header />
       <div className={`${wrapper} my-4`}>
-
-        <Outlet/>
+        <Outlet />
       </div>
       <Footer />
     </Container>
