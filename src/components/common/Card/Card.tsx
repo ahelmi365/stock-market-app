355031b23aecb3025669b407e9f3d0502d732ebc
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 18:21:31 +0200

    add: enhance Card component with props for title, text, and children

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -1,8 +1,19 @@
-
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
+interface ICardProps {
+  title: string;
+  text: string;
+  children?: React.ReactNode;
 }
+const Card = ({ title, text, children }: ICardProps) => {
+  // className="col-sm-12 col-md-6 col-lg-4"
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{title} </h5>
+        <p className="card-text"> {text}</p>
+        {children}
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
