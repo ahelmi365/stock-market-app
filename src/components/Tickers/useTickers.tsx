87ee0a58f05fece7163be476688531b137b7a3ad
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 17:45:57 +0200

    add: init useTickers hook for fetching active ticker data

---
+++ src/components/Tickers/useTickers.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import getTickers from "api/getTickers";
+const useTickers = () => {
+  const url = `https://api.polygon.io/v3/reference/tickers?active=true&limit=9&apiKey=${apiKey}2`;
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["tickers", url],
+    queryFn: () => getTickers(url),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+
+  return { isPending, error, data };
+};
+
+export default useTickers;
