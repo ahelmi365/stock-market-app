95ed0317824f824a5a9751e8153b040599392856
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 15:41:13 +0200

    add SplashScreen component with Nasdaq logo

---
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,13 @@
+import nasdaqLogo from "@assets/nasdaqLogo.svg";
+const SplashScreen = () => {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+     
+        <img src={nasdaqLogo} alt="Nasdaq Logo" style={{ width: "300px", height:"100%" }} />
+     
+      <p className="mt-auto text-dark fw-bold">Devloped By: Ali Helmi</p>
+    </div>
+  );
+};
+
+export default SplashScreen;
