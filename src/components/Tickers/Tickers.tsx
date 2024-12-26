ebd1d43a7209a83292e9e8a0e4f338370b99be4d
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 18:21:57 +0200

    add: render tickers using Card component in Tickers component

--- src/components/Tickers/Tickers.tsx
+++ src/components/Tickers/Tickers.tsx
@@ -1,11 +1,26 @@
 import useTickers from "./useTickers";
 
+import Card from "@components/common/Card/Card";
+
 const Tickers = () => {
   const { isPending, error, data } = useTickers();
-  console.log(data)
-    if (isPending) return <div>Loading...</div>;
+  console.log(data);
   if (error) throw error;
-  return <div className="row"> data</div>;
+  if (isPending) return <div>Loading...</div>;
+  const renderedTickers = data?.results.map((ticker) => (
+    <div className="col-sm-12 col-md-6 col-lg-4" key={ticker.ticker}>
+      <Card title={ticker.ticker} text={ticker.name}>
+        <p>
+          {ticker.currency_symbol} {ticker.base_currency_symbol}
+        </p>
+      </Card>
+    </div>
+  ));
+  return (
+    <div className="row g-3">
+      {renderedTickers}
+    </div>
+  );
 };
 
 export default Tickers;
