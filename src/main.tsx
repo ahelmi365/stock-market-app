15941a340ae05804858cc3cd478ce3b132b2153a
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 17:45:20 +0200

    integrate React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+// tanstack
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 // boostrap
 import "bootstrap/dist/css/bootstrap.min.css";
 // css
 // import "@styles/global.css";
 import AppRouter from "@routes/AppRouter";
-
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );
