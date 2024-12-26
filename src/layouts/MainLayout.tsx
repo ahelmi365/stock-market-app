21a0c304cc7c11ecb07484cbe50f0946f5231175
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 15:51:52 +0200

    add loading state and splash screen to MainLayout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,25 @@
-import { Footer, Header } from "@components/common";
 import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
 import styles from "./styles.module.css";
+import { Header, Footer } from "@components/common";
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SplashScreen from "@components/SplashScreen/SplashScreen";
 const { container, wrapper } = styles;
 const MainLayout = () => {
+  const [loading, setLoading] = useState(true);
 
- 
+  useEffect(() => {
+    // Simulate a loading delay
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <SplashScreen />;
+  }
   return (
     <Container className={container}>
       <Header />
