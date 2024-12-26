ec7b5dba86c521ed3efa62595124b8e86377546e
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 15:16:49 +0200

    refactor main entry point to use AppRouter and update import statements

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 // boostrap
 import "bootstrap/dist/css/bootstrap.min.css";
+// css
+// import "@styles/global.css";
+import AppRouter from "@routes/AppRouter";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppRouter />
+  </StrictMode>
+);
