03363a9cb8905c1e5a2bb932f492207a11064f8c
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 20:53:29 +0200

    fix: improve error handling in getTickers function

--- src/api/getTickers.ts
+++ src/api/getTickers.ts
@@ -9,12 +9,18 @@ interface ITickersResponse {
 
 const getTickers = async (url: string): Promise<ITickersResponse> => {
   const response = await fetch(url);
+  const retryAfter = response.headers.get("Retry-After");
+  console.log({retryAfter})
   if (response.ok) {
     const data = await response.json();
     return data;
   } else {
-    console.log("HTTP Error: ", response.status, response.statusText);
-    throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`);
+    const errorData = await response.json();
+    throw new Error(
+      errorData.error ||
+        `HTTP Error: ${response.status} - ${response.statusText}`
+    );
+    // throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`);
   }
 };
 
