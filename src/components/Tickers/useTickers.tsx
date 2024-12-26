e5d11a0bde20719bd65b3e6a15a3d5b9216675a9
Ali Helmi <ahelmi365@gmail.com>
2024-12-26 20:55:04 +0200

    fix: refactor useTickers to support infinite scrolling

--- src/components/Tickers/useTickers.tsx
+++ src/components/Tickers/useTickers.tsx
@@ -1,17 +1,58 @@
-import { useQuery } from "@tanstack/react-query";
-const apiKey = import.meta.env.VITE_API_KEY;
-
+import { useInfiniteQuery } from "@tanstack/react-query";
 import getTickers from "api/getTickers";
-const useTickers = () => {
-  const url = `https://api.polygon.io/v3/reference/tickers?active=true&limit=9&apiKey=${apiKey}`;
+import { useEffect } from "react";
 
-  const { isPending, error, data } = useQuery({
-    queryKey: ["tickers", url],
-    queryFn: () => getTickers(url),
+const apiKey = import.meta.env.VITE_API_KEY;
+const MaxLimit = 10;
+const initialUrl = `https://api.polygon.io/v3/reference/tickers?active=true&limit=${MaxLimit}&apiKey=${apiKey}`;
+
+const useTickers = () => {
+  const fetchTickers = async ({ pageParam = initialUrl }) => {
+    const response = await getTickers(pageParam);
+    return response;
+  };
+
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     staleTime: 1000 * 60 * 5, // 5 minutes
+    initialPageParam: initialUrl,
+    queryKey: ["tickers", initialUrl],
+    queryFn: fetchTickers,
+    getNextPageParam: (lastPage) =>
+      lastPage.next_url + `&limit=${MaxLimit}&apiKey=${apiKey}` || undefined,
   });
 
-  return { isPending, error, data };
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log("Scroll event triggered");
+      if (
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 2
+      ) {
+        console.log("Bottom of the page reached");
+        if (hasNextPage && !isFetchingNextPage) {
+          console.log("Fetching next page");
+          fetchNextPage();
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  return {
+    data,
+    error,
+    isFetchingNextPage,
+    status,
+  };
 };
 
 export default useTickers;
