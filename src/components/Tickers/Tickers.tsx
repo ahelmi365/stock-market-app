f0e25dfaeb7a55fc7102300c316f4731fa979ecd
Ali Helmi <ahelmi365@gmail.com>
2024-12-27 05:26:11 +0200

    fix: update Tickers component to use tickers directly from useTickers hook

--- src/components/Tickers/Tickers.tsx
+++ src/components/Tickers/Tickers.tsx
@@ -3,21 +3,20 @@ import useTickers from "./useTickers";
 import Card from "@components/common/Card/Card";
 
 const Tickers = () => {
-  const { data, error, isFetchingNextPage, status } = useTickers();
+  const { tickers, error, isFetchingNextPage, status } = useTickers();
   if (status === "pending") return <div>Loading...</div>;
   if (status === "error") throw error;
 
-  const renderedTickers = data?.pages.flatMap((page) =>
-    page.results.map((ticker) => (
-      <div className="col-sm-12 col-md-6 col-lg-4" key={ticker.ticker}>
-        <Card title={ticker.ticker} text={ticker.name}>
-          <p>
-            {ticker.currency_symbol} {ticker.base_currency_symbol}
-          </p>
-        </Card>
-      </div>
-    ))
-  );
+  const renderedTickers = tickers.map((ticker) => (
+    <div className="col-sm-12 col-md-6 col-lg-4" key={ticker.ticker}>
+      <Card title={ticker.ticker} text={ticker.name}>
+        <p>
+          {ticker.currency_symbol} {ticker.base_currency_symbol}
+        </p>
+      </Card>
+    </div>
+  ));
+
   return (
     <div className="row g-3">
       {renderedTickers}
