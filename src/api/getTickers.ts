8cdb3656070c5b10ceca4256416a6ad774d986cf
Ali Helmi <ahelmi365@gmail.com>
2024-12-27 05:25:59 +0200

    fix: remove unused retryAfter logging in getTickers function

--- src/api/getTickers.ts
+++ src/api/getTickers.ts
@@ -10,8 +10,6 @@ interface ITickersResponse {
 
 const getTickers = async (url: string): Promise<ITickersResponse> => {
   const response = await fetch(url);
-  const retryAfter = response.headers.get("Retry-After");
-  console.log({retryAfter})
   if (response.ok) {
     const data = await response.json();
     return data;
