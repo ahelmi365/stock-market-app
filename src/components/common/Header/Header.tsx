da8b9f7773d1533a87b4529c5596221db6062e9d
Ali Helmi <ahelmi365@gmail.com>
2024-12-27 07:58:26 +0200

    feat: add SearchTickers component to Header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -4,34 +4,21 @@ import { NavLink } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 
 import styles from "./styles.module.css";
+import SearchTickers from "@components/SearchTickers/SearchTickers";
 
 function Header() {
   return (
     <header className="mt-4">
       <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={NavLink} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/categories">
-                Categories
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/about-us">
-                About Us
-              </Nav.Link>
-            </Nav>
-            <Nav className="">
-              <Nav.Link as={NavLink} to="/login">
-                Login
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/register">
-                Register
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+        <Container className="d-flex">
+          <Nav className="">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+          </Nav>
+          <div style={{ margin: "auto" }}>
+            <SearchTickers />
+          </div>
         </Container>
       </Navbar>
     </header>
