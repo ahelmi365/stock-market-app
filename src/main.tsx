e14eb28f485093a0c37d567e2f4346a593ea3ddc
Ali Helmi <ahelmi365@gmail.com>
2024-12-27 07:02:59 +0200

    feat: integrate Redux with persistence in main application entry

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+// redux and redux persist
+import { Provider } from "react-redux";
+import { store, presistor } from "@store/store";
+import { PersistGate } from "redux-persist/integration/react";
 // boostrap
 import "bootstrap/dist/css/bootstrap.min.css";
 // css
@@ -12,10 +16,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from "@routes/AppRouter";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={presistor}>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </PersistGate>
+  </Provider>
 );
