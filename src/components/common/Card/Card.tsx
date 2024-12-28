9652162827074bccb3a4c1b49ce0160d896116e2
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 17:02:09 +0200

    feat: enhance accessibility in Card component with ARIA attributes

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -4,9 +4,12 @@ interface ICardProps {
   children?: React.ReactNode;
 }
 const Card = ({ title, text, children }: ICardProps) => {
-  // className="col-sm-12 col-md-6 col-lg-4"
   return (
-    <div className="card h-100 shadow">
+    <div
+      className="card h-100 shadow"
+      role="region"
+      aria-labelledby={`card-title-${title}`}
+    >
       <div className="card-body">
         <h5 className="card-title">{title} </h5>
         <p className="card-text"> {text}</p>
