14d901ebc62dcada88d93f79c73319b37396147c
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 14:47:31 +0200

    feat: add shadow to Card component for improved visual depth

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -6,7 +6,7 @@ interface ICardProps {
 const Card = ({ title, text, children }: ICardProps) => {
   // className="col-sm-12 col-md-6 col-lg-4"
   return (
-    <div className="card h-100">
+    <div className="card h-100 shadow">
       <div className="card-body">
         <h5 className="card-title">{title} </h5>
         <p className="card-text"> {text}</p>
