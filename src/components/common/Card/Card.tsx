00c248b3347eafe024eb7f83878c2554f30df4e5
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 08:51:26 +0200

    feat: update Card component to use full height and adjust title alignment

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -6,9 +6,9 @@ interface ICardProps {
 const Card = ({ title, text, children }: ICardProps) => {
   // className="col-sm-12 col-md-6 col-lg-4"
   return (
-    <div className="card">
+    <div className="card h-100">
       <div className="card-body">
-        <h5 className="card-title text-center">{title} </h5>
+        <h5 className="card-title">{title} </h5>
         <p className="card-text"> {text}</p>
         {children}
       </div>
