88e63fa59601c88e42c83c0382882bbff6784485
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 07:38:53 +0200

    feat: integrate TickersSearchResults component and useHome hook in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import ErrorBoundry from "@components/ErrorBoundery/ErrorBoundery";
 import Tickers from "@components/Tickers/Tickers";
+import TickersSearchResults from "@components/TickersSearchResults/TickersSearchResults";
+import useHome from "./useHome";
 
 const Home = () => {
+  const searchTickersInput = useHome();
   return (
     <ErrorBoundry>
-      <Tickers />
+      {searchTickersInput.length > 0 ? <TickersSearchResults /> : <Tickers />}
     </ErrorBoundry>
   );
 };
