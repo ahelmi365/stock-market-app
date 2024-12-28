353559a76dea2b6cf160e676130b09a1ebd7e143
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 08:51:39 +0200

    feat: comment out currency symbol display in TickersSearchResults component

--- src/components/TickersSearchResults/TickersSearchResults.tsx
+++ src/components/TickersSearchResults/TickersSearchResults.tsx
@@ -11,9 +11,9 @@ const TickersSearchResults = () => {
   const renderedTickers = searchTickersResults.map((ticker) => (
     <div className="col-sm-12 col-md-6 col-lg-4" key={ticker?.ticker!}>
       <Card title={ticker?.ticker!} text={ticker?.name!}>
-        <p>
-          {ticker?.currency_symbol!} {ticker?.base_currency_symbol!}
-        </p>
+        {/* <p className="more-info">
+          {ticker?.currency_symbol! || "---"} {ticker?.base_currency_symbol! || "---"}
+        </p> */}
       </Card>
     </div>
   ));
