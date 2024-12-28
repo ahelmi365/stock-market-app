c8aa08effb2bc1b6fc9b9faa8b6d8e9968d5b502
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 14:27:35 +0200

    fix: implement retry logic for API calls to handle rate limiting

--- src/api/getTickers.ts
+++ src/api/getTickers.ts
@@ -1,18 +1,24 @@
 import { ITickersResponse } from "@customTypes/ticker";
 
 const getTickers = async (url: string): Promise<ITickersResponse> => {
-  const response = await fetch(url);
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  } else {
-    const errorData = await response.json();
-    throw new Error(
-      errorData.error ||
-        `HTTP Error: ${response.status} - ${response.statusText}`
-    );
-    // throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`);
+  let delay = 10000; // 10 seconds
+  let response = null;
+  for (let i = 0; i < 5; i++) {
+    console.log("call api for number:", i + 1);
+    response = await fetch(url);
+
+    if (response.status === 429) {
+      console.log("Excced rate limits");
+      await new Promise((resolve) => setTimeout(resolve, delay + 10000));
+    } else {
+      return response.json(); // Successful response
+    }
   }
+  const errorData = await response?.json();
+  throw new Error(
+    errorData.error ||
+      `HTTP Error: ${response?.status} - ${response?.statusText}`
+  );
 };
 
 export default getTickers;
