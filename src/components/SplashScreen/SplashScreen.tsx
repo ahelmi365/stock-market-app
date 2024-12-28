823acdcca302976657a36e9baa6a99035879d592
Ali Helmi <ahelmi365@gmail.com>
2024-12-28 14:38:54 +0200

    feat: add Loading component to SplashScreen for enhanced user experience during loading

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -1,11 +1,15 @@
 import nasdaqLogo from "@assets/nasdaqLogo.svg";
+import Loading from "@components/common/Loading/Loading";
 const SplashScreen = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-     
-        <img src={nasdaqLogo} alt="Nasdaq Logo" style={{ width: "300px", height:"100%" }} />
-     
-      <p className="mt-auto text-dark fw-bold">Developed By: Ali Helmi</p>
+      <img
+        src={nasdaqLogo}
+        alt="Nasdaq Logo"
+        style={{ width: "300px", height: "100%" }}
+      />
+      <Loading />
+      <p className="mt-auto text-dark fw-bold m-4">Developed By: Ali Helmi</p>
     </div>
   );
 };
