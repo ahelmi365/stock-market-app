17d0ff53af79e6409f17387602a49a24a9ef514f
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 10:05:02 +0200

    feat: replace Card component with GridList for displaying tickers

--- src/components/Tickers/Tickers.tsx
+++ src/components/Tickers/Tickers.tsx
@@ -1,29 +1,18 @@
-import { sliceLongText } from "utils";
 import useTickers from "./useTickers";
 
-import Card from "@components/common/Card/Card";
 import Loading from "@components/common/Loading/Loading";
+import GridList from "@components/GridList/GridList";
 
 const Tickers = () => {
   const { tickers, error, isFetchingNextPage, status } = useTickers();
-  if (status === "pending") return <Loading/>;
+  if (status === "pending") return <Loading />;
   if (status === "error") throw error;
-
-  const renderedTickers = tickers.map((ticker) => (
-    <div className="col-sm-12 col-md-6 col-lg-4" key={ticker.ticker}>
-      <Card title={ticker.ticker} text={sliceLongText(ticker?.name!)}>
-        {/* <p className="nore-info">
-          {ticker.currency_symbol} {ticker.base_currency_symbol}
-        </p> */}
-      </Card>
-    </div>
-  ));
 
   return (
     <div className="row g-3">
       <h5>All Tickers</h5>
-      {renderedTickers}
-      {isFetchingNextPage && <Loading/>}
+      <GridList tickers={tickers} />
+      {isFetchingNextPage && <Loading />}
     </div>
   );
 };
