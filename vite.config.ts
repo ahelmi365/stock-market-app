e280492935d8df8d423ec9d9c0b74f175adf10f0
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 03:22:23 +0200

    fix: remove redundant comma in Vite configuration and clean up coverage settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), , tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+
   test: {
     environment: "happy-dom",
-    coverage: {
-      reporter: ["text", "json", "html"], // this object
-    },
   },
-})
+});
