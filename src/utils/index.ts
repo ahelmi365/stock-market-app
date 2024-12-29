0fcebe0e197e71945222c6c0da46dfc8dca98e30
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 11:23:16 +0200

    feat: implement fetchTickers function for optimized ticker retrieval and storage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
-import { ITickersResponse } from "@customTypes/ticker";
+import { ITickersResponse, IQueryParams } from "@customTypes/ticker";
+import getTickers from "api/getTickers";
 
 // Custom debounce function
 export function debounce<T extends (...args: string[]) => void>(
@@ -34,4 +35,25 @@ export const storeHasTicker = (
     }
   });
   return results.some((result) => result === true);
+};
+
+export const fetchTickers = async ({
+  pageParam,
+  tickersFromTheStore,
+  setTickersInTheStore,
+}: IQueryParams) => {
+  // check if there is a response in the store with this url or not
+  if (storeHasTicker(pageParam, tickersFromTheStore)) {
+    console.log("Store already has this ticker");
+    const targetResponse = tickersFromTheStore.filter(
+      (response) => response[pageParam] != undefined
+    );
+    return targetResponse[0][pageParam];
+  } else {
+    // call api to get new response
+    console.log("get new ticker and add it to the store");
+    const response = await getTickers(pageParam);
+    setTickersInTheStore(response, pageParam);
+    return response;
+  }
 };
