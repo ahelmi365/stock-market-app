1341f19d6be1c91c9ebe9b2745d6c6ce0fe489cc
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 03:23:28 +0200

    test: add unit tests for Header component to verify home link functionality

---
+++ src/__tests__/Header.test.tsx
@@ -0,0 +1,30 @@
+import { Header } from "@components/common";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import configureMockStore from "redux-mock-store";
+import { expect, test } from "vitest";
+
+// setupTests.ts
+// import '@testing-library/jest-dom';
+const mockStore = configureMockStore();
+const store = mockStore({
+  tickers: {
+    responses: [],
+  },
+});
+
+test("just a test", () => {
+  const screen = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  const homeLink = screen.getByRole("link", { name: /home/i });
+  expect(homeLink.innerText).toContain("Home");
+  expect(homeLink.hasAttribute("href"));
+  expect(homeLink.getAttribute("href")).toBe("/");
+});
