880c90efacba7975af86f036636e32560adbe925
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 14:21:03 +0200

    add const MAX_NUMBER_OF_HOME_PAGE_RESPONSES

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,4 @@
-export const MAX_NUMBER_OF_SEARCH_RESPONSES = 50;
+export const MAX_NUMBER_OF_HOME_PAGE_RESPONSES = 10; // 10 * 10 = 100 tickers
+export const MAX_NUMBER_OF_SEARCH_RESPONSES = 50; // 50 * 10 = 500 tickers
 export const MAX_NUMBER_OF_RESPONSES_PER_SEARCH_TEXT = 5;
 export const QUERY_STALE_TIME = 1000 * 60 * 5 // 5 MINS
