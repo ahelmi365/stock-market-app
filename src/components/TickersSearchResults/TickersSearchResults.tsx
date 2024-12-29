69ac711561c3f2672ad232e303d2e68b29867f89
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 10:05:08 +0200

    feat: replace Card component with GridList in TickersSearchResults for improved layout

--- src/components/TickersSearchResults/TickersSearchResults.tsx
+++ src/components/TickersSearchResults/TickersSearchResults.tsx
@@ -1,31 +1,28 @@
-import { sliceLongText } from "utils";
 import useTickersSearchResults from "./useTickersSearchResults";
 
-import Card from "@components/common/Card/Card";
 import Loading from "@components/common/Loading/Loading";
+import GridList from "@components/GridList/GridList";
 
 const TickersSearchResults = () => {
   const { searchTickersResults, error, isFetchingNextPage, status } =
     useTickersSearchResults();
-  if (status === "pending") return <Loading/>;
+  if (status === "pending") return <Loading />;
   if (status === "error") throw error;
-
-  const renderedTickers = searchTickersResults.map((ticker) => (
-    <div className="col-sm-12 col-md-6 col-lg-4" key={ticker?.ticker!}>
-      <Card title={ticker?.ticker!} text={sliceLongText(ticker?.name!)}>
-        {/* <p className="more-info">
-          {ticker?.currency_symbol! || "---"} {ticker?.base_currency_symbol! || "---"}
-        </p> */}
-      </Card>
-    </div>
-  ));
 
   return (
     <div className="row g-3">
       <h5>Search Results</h5>
 
-      {searchTickersResults.length>0? renderedTickers: <p>No results found!</p>}
-      {isFetchingNextPage && <Loading/>}
+      {searchTickersResults.length > 0 ? (
+        <GridList
+          tickers={searchTickersResults.filter(
+            (ticker) => ticker !== undefined
+          )}
+        />
+      ) : (
+        <p>No results found!</p>
+      )}
+      {isFetchingNextPage && <Loading />}
     </div>
   );
 };
