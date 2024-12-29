d2cf363db914fbb0ac8b0cb50a115024db454386
Ali Helmi <ahelmi365@gmail.com>
2024-12-29 03:23:58 +0200

    fix: add data-testid attribute to Footer component for testing purposes

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const { footerContainer } = styles;
 const thisYear = new Date().getFullYear();
 const Footer = () => {
   return (
-    <footer className={footerContainer}>
+    <footer className={footerContainer} data-testid="footer">
       © {thisYear} Stock Market. All rights reserved.
     </footer>
   );
